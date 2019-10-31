2ddb1204eaa58791d145fd34cee03f99b03bf282
mike <mike.schoenbeck@gmail.com>
2019-10-31 14:24:13 -0400

    created classroom constructors and getters and getAverageExamScore()

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,42 @@
 package io.zipcoder;
 
 public class Classroom {
+    Student[] students;
+    int maxNumberOfStudents;
+
+
+    Classroom (int maxNumberOfStudents) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    Classroom (int maxNumberOfStudents, Student [] students)    {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+        this.students = students;
+    }
+
+    Classroom ()    {
+        Student[] student = new Student [30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public double getAverageExamScore(double[]examScores) {
+        double avgExamScore = 0;
+
+        for (int i = 0; i < examScores.length; i++) {
+            avgExamScore += examScores[i];
+        }   return avgExamScore / students.length;
+
+}
+
+
+
+
+
+
+
+
+
 }
