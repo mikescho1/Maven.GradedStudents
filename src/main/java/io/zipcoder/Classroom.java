0037f90412648cec6b4a08997580b4c1d1cc90c7
mike <mike.schoenbeck@gmail.com>
2019-10-31 21:09:48 -0400

    completed removeStudent()

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,22 +1,23 @@
 package io.zipcoder;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Classroom {
     private Student[] students;         // this is an array of students.
     private int maxNumberOfStudents; //this represents the max length of the students array.
 
-    Classroom ()    {
-        this.students = new Student [30];
-    }
-    Classroom (int maxNumberOfStudents) {
-        this.students = new Student [maxNumberOfStudents];
+    Classroom() {
+        this.students = new Student[30];
     }
 
-    Classroom (Student [] students)    {
-         this.students = students;
+    Classroom(int maxNumberOfStudents) {
+        this.students = new Student[maxNumberOfStudents];
     }
 
+    Classroom(Student[] students) {
+        this.students = students;
+    }
 
 
     public Student[] getStudents() {
@@ -29,28 +30,35 @@ public class Classroom {
         for (Student student : students) {
             for (double score : student.getListOfScores()) {
                 sum += score;
-            }   counter += student.getNumberOfExamsTaken();
+            }
+            counter += student.getNumberOfExamsTaken();
         }
         return sum / counter;
     }
 
     public void addStudent(Student student) {
-        Student [] students2 = new Student[students.length + 1];
+        Student[] students2 = new Student[students.length + 1];
         students2[this.students.length] = student;
         students = students2;
-
-
-
-
     }
 
 
+    public void removeStudent(String firstName, String lastName) {
+        Student[] students1 = new Student[students.length - 1];
+        int identifier = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getFirstName().equals(firstName) && students[i].getLastName().equals(lastName)) {
+                identifier = i;
+            }
+        }
+        for (int i = 0; i < students1.length; i++) {
+            if (i < identifier) {
+                students1[i] = students[i];
+            }
+            if (i > identifier) {
+                students[i - 1] = students[i];
 
-
-
-
-
-
-
-
+            }
+        }
+    }
 }
